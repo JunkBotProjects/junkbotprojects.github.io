7cc22d60d544ebe0cc06b6cf7fe3bd6c51c093ae
Athul Raj <athulbraj@gmail.com>
2016-07-09 11:01:38 +0530

    Auto

--- update.bat
+++ update.bat
@@ -1,3 +1,15 @@
+set count=
+for /d %%a in (H:\junkbotprojects.github.io\projects\*) do set /a count+=1
+echo %count%
+
+@echo { > site.conf
+@echo 	"site":{ >> site.conf
+@echo 		"project":{ >> site.conf
+@echo 			"count" : %count% >> site.conf
+@echo 		} >> site.conf
+@echo 	} >> site.conf
+@echo } >> site.conf
+
 git pull origin master
 git add .
 git commit -m "Auto"
