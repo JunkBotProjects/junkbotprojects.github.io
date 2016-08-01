fdcb60afe3bbc5da0d4203b0a42446a6cf035880
Athul Raj <athulbraj@gmail.com>
2016-08-01 19:11:01 +0530

    Auto

--- projects/ir-rem/code.ino
+++ projects/ir-rem/code.ino
@@ -1,41 +1,20 @@
         #include <SoftwareSerial.h>
-        SoftwareSerial SSerial (2,3);
+        #include <IRremote.h>
+        SoftwareSerial BTMod (2,3);
+        int RECV_PIN = 11;
+        IRrecv irrecv(RECV_PIN);
 
-        #include <AFMotor.h>
-        AF_DCMotor motor1(1, MOTOR12_64KHZ);
-        AF_DCMotor motor2(2, MOTOR12_64KHZ);
+        decode_results R1, R2;
 
         void setup() {
           // put your setup code here, to run once:
-          SSerial.begin(9600);
-          motor1.setSpeed(200);
-          motor2.setSpeed(200);
+          BTMod.begin(9600);
+          
         }
 
         void loop() {
-          // put your main code here, to run repeatedly:
-          if(SSerial.available()){
-            switch ( SSerial.read ) {
-              case 'L' :
-                motor1.run(FORWARD);
-                motor2.run(BACKWARD);
-              break;
-              case 'R' :
-                motor1.run(BACKWARD);
-                motor2.run(FORWARD);
-              break;
-              case 'F' :
-                motor1.run(FORWARD);
-                motor2.run(FORWARD);
-              break;
-              case 'B' :
-                motor1.run(BACKWARD);
-                motor2.run(BACKWARD);
-              break;
-              case 'S' :
-                motor1.run(RELEASE);
-                motor2.run(RELEASE);
-              break;
-            }
-          }
+          if(irrecv.decode(&results)){  
+          translateIR(); 
+          irrecv.resume(); // receive the next value
+        } 
         }
